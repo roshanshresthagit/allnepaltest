2554aeeaf76347b486cc221143b8931533bb1776
Roshan <roshanshrestha0921@gmail.com>
2024-04-16 13:17:45 +0545

    design update

--- quizes/views.py
+++ quizes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Category, Subject
+import random
+from .models import Category, Subject,Question
 
 def home(request):
     categories = Category.objects.all()
@@ -12,5 +13,10 @@ def display_subjects(request, category_id):
 
 def quiz_view(request, subject_id):
     quiz = Subject.objects.filter(id=subject_id).first()
-    context ={"quiz":quiz}
+    questions = Question.objects.filter(subject_id=subject_id)
+    selected_questions = random.sample(list(questions),2) 
+    context ={"quiz":selected_questions}
     return render(request,'quiz.html',context)
+
+
+
