d7f0c25ee77f6473d9f839170d8145e20df650d4
Roshan <roshanshrestha0921@gmail.com>
2024-05-17 19:07:51 +0545

    minor code optimization on view

--- quizes/views.py
+++ quizes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 import random
 from .models import Category, Subject,Question
 
@@ -7,12 +7,11 @@ def home(request):
     return render(request, 'index.html', {'categories': categories})
 
 def display_subjects(request, category_id):
-    selected_category = Category.objects.get(pk=category_id)
+    selected_category = get_object_or_404(Category, pk=category_id)
     subjects = selected_category.subject.all()
     return render(request, 'subjects.html', {'category': selected_category, 'subjects': subjects})
 
 def quiz_view(request, subject_id):
-    # quiz = Subject.objects.filter(id=subject_id).first()
     questions = Question.objects.filter(subject_id=subject_id)
     selected_questions = random.sample(list(questions),20) #here to change the number of question to show 
     context ={"quiz":selected_questions}
@@ -20,42 +19,29 @@ def quiz_view(request, subject_id):
 
 def submit_quiz(request):
     if request.method == 'POST':
-        # Process the submitted quiz answers
-        # Retrieve the user's answers from the form
         user_answers = []
-        for question in Question.objects.all():
-            selected_option = request.POST.get(str(question.id), None)
-            if selected_option is not None:
+        for question_id in request.POST:
+            if question_id.isdigit():  # Check if the form field represents a question ID
+                selected_option = request.POST.get(question_id)
                 user_answers.append({
-                    'question_id': question.id,
+                    'question_id': int(question_id),
                     'selected_option': selected_option
                 })
 
-        # Store the user's answers in the session
         request.session['user_answers'] = user_answers
-
-        # Redirect to the quiz results page
         return redirect('quiz_results')
     else:
-        # Handle invalid request method (GET or other methods)
-        # Redirect to an appropriate page or show an error message
+        # Handle invalid request method
         pass
 
 def quiz_results(request):
-    # Retrieve the user's answers from the session
     user_answers = request.session.get('user_answers', [])
+    question_ids = [answer['question_id'] for answer in user_answers]
+    questions = Question.objects.filter(pk__in=question_ids)
     
-    # Calculate the total marks obtained by the user
-    total_marks = 0
-    for answer in user_answers:
-        question = Question.objects.get(pk=answer['question_id'])
-        if question.correct_answer == answer['selected_option']:
-            total_marks += question.marks
+    total_marks = sum(question.marks for question in questions if question.correct_answer == user_answers[question_ids.index(question.id)]['selected_option'])
     
-    # Clear the user's answers from the session
-    request.session['user_answers'] = []
+    request.session['user_answers'] = []  # Clear the user's answers
 
-    # Render the quiz results page with the total marks
     return render(request, 'quiz_results.html', {'total_marks': total_marks})
-
 
